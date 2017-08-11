2da18dfb87d389d02217694f8dfe16b602466cf8
unknown <simon.coding@gmail.com>
2017-08-12 00:14:21 +0800

    ID: Chap7.2.1 DES: 【解析方法符号引用】非接口方法符号引用

--- src/jvmgo/ch07/rtda/heap/cp_memberref.go
+++ src/jvmgo/ch07/rtda/heap/cp_memberref.go
@@ -24,3 +24,61 @@ func (self *MemberRef) Name() string {
 func (self *MemberRef) Descriptor() string {
 	return self.descriptor
 }
+
+/*7.2.1*/
+func (self *MemberRef) ResolvedMethod() *Method {
+    if self.method == nil {
+        self.resolvedMethodRef()
+    }
+    return self.method
+}
+
+func (self *MemberRef) resolvedMethodRef() {
+    d := self.cp.class
+    c := self.ResolvedClass()
+    if c.IsInterface() {
+        panic("java.lang.IncomptiableClassChangeError");
+    }
+    method := lookupMethod(c, self.name, self.descriptor)
+    if method == nil {
+        panic("java.lang.NoSuchMethodError")
+    }
+    if !method.isAccessibleTo(d) {
+        panic("java.lang.IllegalAccessError")
+    }
+    self.method = method
+}
+
+func lookupMethod(class *Class, name, descriptor string) *Method {
+    method := lookupMethodInClass(class, name, descriptor)
+    if method == nil {
+        method = lookupMethodInInterfaces(class.interfaces, name, descriptor)
+    }
+    return method
+}
+
+func lookupMethodInClass(class *Class, name, descriptor string) *Method {
+    for c := class; c != nil ; c = c.superClass {
+        for _, method := range c.methods {
+            if method.name == name && method.descriptor == descriptor {
+                return method
+            }
+        }
+    }
+    return nil
+}
+
+func lookupMethodInInterfaces(ifaces []*Class, name, descriptor string) *Method {
+    for _, iface := range ifaces {
+        for _, method := range ifaces.methods {
+            if method.name == name && method.descriptor == descriptor {
+                return method;
+            }
+        }
+        method := lookupMethodInInterfaces(iface.interfaces, name, descriptor)
+        if method != nil {
+            return method
+        }
+    }
+    return nil
+}
