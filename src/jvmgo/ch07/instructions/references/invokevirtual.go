f8f0ddd488f3e3900e6d668d2005e6c6c010e01f
unknown <simon.coding@gmail.com>
2017-08-13 16:31:52 +0800

    ID: Chap 7.5.3 DES: invokevirtual指令

--- src/jvmgo/ch07/instructions/references/invokevirtual.go
+++ src/jvmgo/ch07/instructions/references/invokevirtual.go
@@ -6,17 +6,55 @@ import "jvmgo/ch07/rtda"
 import "jvmgo/ch07/rtda/heap"
 
 // Invoke instance method; dispatch based on class
+/*
+    Chap 6 & 7.5.3
+    invokevirtual指令
+    xinxin.shi
+    2017-08-13 16:31:33
+*/
 
 type INVOKE_VIRTUAL struct{ base.Index16Instruction }
 
 // hack!
 
 func  (self *INVOKE_VIRTUAL) Execute(frame *rtda.Frame) {
-    cp := frame.Method().Class().ConstantPool()
+    currentClass := frame.Method().Class()
+    // cp := frame.Method().Class().ConstantPool()
+    cp := currentClass.ConstantPool()
     methodRef := cp.GetConstant(self.Index).(*heap.MethodRef)
-	if methodRef.Name() == "println" {
-        stack := frame.OperandStack()
-        switch methodRef.Descriptor() {
+    resolvedMethod := methodRef.ResolvedMethod()
+    if resolvedMethod.IsStatic() {
+        panic("java.lang.IncompatibleClassChangeError")
+    }
+    ref := frame.OperandStack().GetRefFromTop(resolvedMethod.ArgSlotCount()-1)
+    if ref == nil {
+        // hack!
+        if methodRef.Name() == "println" {
+            _println(frame.OperandStack(), methodRef.Descriptor());
+            return
+        }
+        panic("java.lang.NullPointerExeception")
+    }
+
+    if resolvedMethod.IsProtected() &&
+        resolvedMethod.Class().IsSuperClassOf(currentClass) &&
+        resolvedMethod.Class().GetPackageName() != currentClass.GetPackageName() &&
+        ref.Class() != currentClass &&
+        !ref.Class().IsSubClassOf(currentClass) {
+            panic("java.lang.IllegalAccessError")
+    }
+
+    methodToBeInvoked := heap.LookupMethodInClass(ref.Class(), 
+            methodRef.Name(), methodRef.Descriptor())
+    if methodToBeInvoked == nil || methodToBeInvoked.IsAbstract() {
+        panic("java.lang.AbstractMethodError")
+    }
+    base.InvokeMethod(frame, methodToBeInvoked)
+
+
+}
+func _println(stack *rtda.OperandStack, descriptor string) {
+        switch descriptor {
             case "(Z)V": 
                 fmt.Printf("%v\n", stack.PopInt() != 0)
             case "(C)V": 
