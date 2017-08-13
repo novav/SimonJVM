4d4b6fc76a9dc21613dc696b176bbca1f0d3bb16
unknown <simon.coding@gmail.com>
2017-08-13 20:45:47 +0800

    ID: Chap7.6 DES: 改进解释器

--- src/jvmgo/ch07/interpreter.go
+++ src/jvmgo/ch07/interpreter.go
@@ -12,13 +12,12 @@ import "jvmgo/ch07/rtda/heap"
     // maxLocals := codeAttr.MaxLocals()
     // maxStack := codeAttr.MaxStack()
     // bytecode := codeAttr.Code()
-func interpret(method *heap.Method) {
-
+func interpret(method *heap.Method, logInst bool) {
     thread := rtda.NewThread()
     frame := thread.NewFrame(method)
     thread.PushFrame(frame)
     defer catchErr(frame)
-    loop(thread, method.Code())
+    loop(thread, logInst)
 }
 
 
@@ -30,20 +29,52 @@ func catchErr(frame *rtda.Frame) {
     }
 }
 
-func loop(thread *rtda.Thread, bytecode []byte) {
-	frame := thread.PopFrame()
+func loop(thread *rtda.Thread, logInst bool) {
     reader := &base.BytecodeReader{}
     for {
+	    frame := thread.PopFrame()
         pc := frame.NextPC()
 		thread.SetPC(pc)
 		// decode
-		reader.Reset(bytecode, pc)
+		reader.Reset(frame.Mehtod().Code(), pc)
         opcode := reader.ReadUint8()
 		inst := instructions.NewInstruction(opcode)
         inst.FetchOperands(reader)
         frame.SetNextPC(reader.PC())
+        if (logInst) {
+            logInstruction(frame, inst)
+        }
         // execute 
         fmt.Printf("pc:%2d inst: %T %v \n", pc, inst, inst)
         inst.Execute(frame)
+        if thread.IsStackEmpty() {
+            break
+        }
     }
+}
+
+
+func catchErr(thread *rtda.Thread) {
+    if r := recover(); r != nil {
+        logFrames(thread)
+        panic(r)
+    }
+}
+
+func logFrames(thread *rtda.Thread) {
+    for !thread.IsStackEmpty() {
+        frame := thread.PopFrame()
+        method := frame.Method()
+        className := method.Class().Name()
+        fmt.Printf(">> pc: %4d %v.%v%v \n", 
+            frame.NextPC(), className, method.Name(), method.Descriptor())
+    }
+}
+
+func logInstruction(frame *rtda.Frame, inst base.Instruction) {
+    method := frame.Method()
+    className := method.Class().Name()
+    methodName := method.Name()
+    pc := frame.Thread().PC()
+    fmt.Printf("%v.%v() #%2d %T %v\n", className, methodName, pc, inst, inst)
 }
--- src/jvmgo/ch07/rtda/thread.go
+++ src/jvmgo/ch07/rtda/thread.go
@@ -45,3 +45,7 @@ func (self *Thread) CurrentFrame() *Frame {
 func (self *Thread) NewFrame(method *heap.Method) *Frame {
 	return newFrame(self, method)
 }
+
+func (self *Thread) IsStackEmpty() bool {
+    return self.stack.isEmpty()
+}
