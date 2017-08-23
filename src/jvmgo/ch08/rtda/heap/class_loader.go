d4fb673ad8aeb978b3d3de3274831a9df848febb
unknown <simon.coding@gmail.com>
2017-08-23 21:47:59 +0800

    ID: Chap 8.5.3 DES: 完善类加载器

--- src/jvmgo/ch08/rtda/heap/class_loader.go
+++ src/jvmgo/ch08/rtda/heap/class_loader.go
@@ -197,7 +197,10 @@ func initStaticFinalVar(class *Class, field *Field) {
                 val := cp.GetConstant(cpIndex).(float64)
                 vars.SetDouble(slotId, val)
             case "Ljava/lang/String;" :
-                panic("todo") // chap 8
+                goStr := cp.GetConstant(cpIndex).(string)
+                jStr := JString(class.Loader(), goStr)
+                vars.SetRef(slotId, jStr)
+                // panic("todo") // chap 8
         }
     }
 }
