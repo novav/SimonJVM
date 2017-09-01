d5833949c198591894675bbc8296edf0dc4880c8
unknown <simon.coding@gmail.com>
2017-09-01 21:50:08 +0800

    ID: Chap9.5 DES: HashCode() & equals() & toString()

--- src/jvmgo/ch09/native/java/lang/Object.go
+++ src/jvmgo/ch09/native/java/lang/Object.go
@@ -1,5 +1,6 @@
 package lang
 
+import "unsafe"
 import "jvmgo/ch09/native"
 import "jvmgo/ch09/rtda"
 
@@ -9,8 +10,11 @@ import "jvmgo/ch09/rtda"
     xinxin.shi
     2017-08-27 14:40:47
 */
+const jlObject = "java/lang/Object"
+
 func init() {
-    native.Register("java/lang/Object", "getClass", "()Ljava/lang/Class;", getClass)
+    native.Register(jlObject, "getClass", "()Ljava/lang/Class;", getClass)
+    native.Register(jlObject, "hashCode", "()I", hashCode)
 }
 
 // public final native Class<?> getClass();
@@ -19,4 +23,10 @@ func getClass(frame *rtda.Frame) {
     this := frame.LocalVars().GetThis()
     class := this.Class().JClass()
     frame.OperandStack().PushRef(class)
+}
+
+func hashCode(frame *rtda.Frame) {
+    this := frame.LocalVars().GetThis()
+    hash := int32(uintptr(unsafe.Pointer(this)))
+    frame.OperandStack().PushInt(hash)
 }
