392c4cd50ca691ac19412f89a7aeadef62a00ae8
unknown <simon.coding@gmail.com>
2017-09-01 22:05:15 +0800

    ID: Chap9.5 DES: Object.Clone()

--- src/jvmgo/ch09/native/java/lang/Object.go
+++ src/jvmgo/ch09/native/java/lang/Object.go
@@ -15,6 +15,7 @@ const jlObject = "java/lang/Object"
 func init() {
     native.Register(jlObject, "getClass", "()Ljava/lang/Class;", getClass)
     native.Register(jlObject, "hashCode", "()I", hashCode)
+    native.Register(jlObject, "clone", "()Ljava/lang/Object;", clone)
 }
 
 // public final native Class<?> getClass();
@@ -25,8 +26,21 @@ func getClass(frame *rtda.Frame) {
     frame.OperandStack().PushRef(class)
 }
 
+// public native int hashCode();
+// ()I
 func hashCode(frame *rtda.Frame) {
     this := frame.LocalVars().GetThis()
     hash := int32(uintptr(unsafe.Pointer(this)))
     frame.OperandStack().PushInt(hash)
+}
+
+// protected native Object clone() throws CloneNotSupportedException;
+// ()Ljava/lang/Object;
+func clone(frame *rtda.Frame) {
+    this := frame.LocalVars().GetThis()
+    cloneable := this.Class().Loader().LoadClass("java/lang/Cloneable")
+    if !this.Class().IsImplements(cloneable) {
+        panic("java.lang.CloneNotSupportedException")
+    }
+    frame.OperandStack().PushRef(this.Clone())
 }
