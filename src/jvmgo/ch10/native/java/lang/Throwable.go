98266a89eaa6999d32c982577dfba3feaf4ae6fd
unknown <simon.coding@gmail.com>
2017-09-08 21:27:37 +0800

    ID: Chap10.5 DES: Java虚拟机栈信息

--- src/jvmgo/ch10/native/java/lang/Throwable.go
+++ src/jvmgo/ch10/native/java/lang/Throwable.go
@@ -10,6 +10,14 @@ import "jvmgo/ch10/rtda/heap"
     xinxin.shi
     2017-09-04 22:36:45
 */
+
+type StackTraceElement struct {
+    fileName        string
+    className       string
+    methodName      string
+    lineNumber      int
+}
+
 func init() {
     native.Register("java/lang/Throwable", "fillInStackTrace",
         "(I)Ljava/lang/Throwable;", fillInStackTrace)
@@ -18,4 +26,18 @@ func init() {
 // private native Throwable fillInStackTrace(int dummy)
 func fillInStackTrace(frame *rtda.Frame) {
     // 10.5 
+    this := frame.LocalVars().GetThis()
+    frame.OperandStack().PushRef(this)
+    stes := createStackTraceElements(this, frame.Thread())
+    this.SetExtra(stes)
+}
+
+func createStackTraceElements(tObj *heap.Object, thread *rtda.Thread) {
+    skip := distanceToObject(tObj.Class()) + 2
+    frames := thread.GetFrames()[skip:]
+    stes := make([]*StackTraceElement, len(frames))
+    for i, frame := range frames {
+        stes[i] = createStackTraceElements(frame)
+    }
+    return stes
 }
